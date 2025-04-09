014f0961c80e6dbc5ea92da2a5bed02546cda7ba
Denissedz101 <de.diazm@duocuc.cl>
2025-04-09 17:52:46 -0400

    mejora visualizacion django

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\denis\E11evenStore-Django\env
+set VIRTUAL_ENV=C:\Users\denis\OneDrive\Escritorio\E11even-Store\env
 
 if not defined PROMPT set PROMPT=$P$G
 
