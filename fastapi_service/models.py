f0e14c8b1e1a54e25c8934eaef708c1bde15e89f
Denissedz101 <de.diazm@duocuc.cl>
2025-04-27 11:27:28 -0400

    ajustes footer

--- fastapi_service/models.py
+++ fastapi_service/models.py
@@ -5,7 +5,7 @@ from .database import Base
 # PRODUCTO
 class Producto(Base):
     __tablename__ = "E11EVENSTORE_PRODUCTO"
-
+    
     id = Column(Integer, primary_key=True, autoincrement=True)
     nombre = Column(String(100))
     descripcion = Column(String(500), nullable=True)
